85acf1fb9d0285ae844889897b7c946d6128e2e4
Weiming Zhai <wmzhai@gmail.com>
2017-03-13 15:07:04 +0800

    echo

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-const http = require('http');
+var http = require('http');
 
-// 创建一个web server对象
-const server = http.createServer();
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+    // BEGINNING OF NEW STUFF
 
-// 添加request handler
-// 当HTTP request到达服务器时，node调用这个request handler来处理请求
-server.on('request', function(request, response) {
-  console.log('on request');
-});
+    response.on('error', function(err) {
+      console.error(err);
+    });
 
-// 服务需要监听一个端口来响应请求
-server.listen(3000,()=>{
-  console.log('Server listening port 3000');
-});
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.writeHead(200, {'Content-Type': 'application/json'})
+
+    var responseBody = {
+      headers: headers,
+      method: method,
+      url: url,
+      body: body
+    };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.end(JSON.stringify(responseBody))
+
+    // END OF NEW STUFF
+  });
+}).listen(8080);
